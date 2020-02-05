e05d2eb5fa135df43bc007365751810dee4b0146
Paul Debevec <paulmdebevec@gmail.com>
2020-02-05 16:21:50 -0700

    Add sessions and add pet to favorites functionality

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,6 +5,7 @@ class FavoritesController < ApplicationController
     favorites = Favorite.new(session[:favorites])
     favorites.add_pet(pet.id)
     session[:favorites] = favorites.contents
+    flash[:success] = "#{pet.name} Was Favorited!"
     redirect_to "/pets/#{pet.id}"
   end
 end
