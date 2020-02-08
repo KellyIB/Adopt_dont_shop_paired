6b6610954fcf93d8cf9299a2714b7b29839904d9
KellyIB <kellyibard@gmail.com>
2020-02-08 10:10:19 -0700

    added functionality for user story 17. error flash message
    Co-Authored-By: Paul Debevec <paulmdebevec@gmail.com>

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,30 @@
+class ApplicationsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    check_pets = Pet.where(id: params[:favorites])
+    application = Application.new(application_params)
+    if application.save
+      check_pets.each do |pet|
+        pet.applications << application
+        favorites.remove_pet(pet.id)
+      end
+      redirect_to "/favorites"
+      flash[:success] = "Your Application Was Submitted!"
+    else
+      flash[:error] = application.errors.full_messages.to_sentence
+      redirect_to "/favorites/applications/new"
+    end
+  end
+
+  private
+
+  def application_params
+    params.permit(:name, :address, :city, :state, :zip, :phone_number, :description)
+  end
+
+
+end
