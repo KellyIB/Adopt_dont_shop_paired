7df24f7f07b356d0fb6af654a3d6800064196d20
Paul Debevec <paulmdebevec@gmail.com>
2020-02-09 15:32:41 -0700

    Add revoke method to revoke application
    Co-Authored-By: Kelly Bard <kellyibard@gmail.com>

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -12,5 +12,12 @@ class ApplicationPetsController < ApplicationController
     redirect_to "/pets/#{@pet.id}"
   end
 
+  def revoke
+    @pet = Pet.find(params[:pet_id])
+    @pet.update_column(:adoptable?, true)
+    @applications_pet = ApplicationPet.where(pet_id: (params[:pet_id]), application_id: (params[:application_id])).first
+    @applications_pet.update_column(:approved?, false)
+    redirect_to "/applications/#{params[:application_id]}"
+  end
 
 end
