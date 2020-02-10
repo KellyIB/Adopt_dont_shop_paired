b81503f1a31ad4805bc7b42a8187d2b06d6f6df2
KellyIB <kellyibard@gmail.com>
2020-02-10 12:19:44 -0700

    change rating to integer from string for user story 30
    Co-Authored-By: Paul Debevec <paulmdebevec@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200209171804) do
+ActiveRecord::Schema.define(version: 20200210011733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,12 +50,12 @@ ActiveRecord::Schema.define(version: 20200209171804) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
-    t.string "rating"
     t.string "content"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
+    t.integer "rating"
     t.index ["shelter_id"], name: "index_reviews_on_shelter_id"
   end
 
